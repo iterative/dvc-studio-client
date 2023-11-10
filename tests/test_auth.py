5ff3de0ab6ca871ccff41e1c1b3498947c8c6d37
Amrit Ghimire <mail@amritghimire.com>
2023-11-10 18:23:40 +0545

    Add authorization methods in auth.py
    This commit introduces two new methods, 'start_device_login' and 'check_token_authorization', in the auth.py file. These methods initiate the device login process for Studio and check the token authorization using a device code. Used the urllib, requests, and os modules, and defined the necessary exceptions.
    
    This new module will be used in https://github.com/iterative/dvc/pull/10074

---
+++ tests/test_auth.py
@@ -0,0 +1,111 @@
+import pytest
+import requests
+from requests import Response
+
+from dvc_studio_client.auth import start_device_login, check_token_authorization
+
+
+def test_start_device_login(mocker):
+    mock_post = mocker.patch(
+        "requests.post",
+        return_value=mock_response(mocker, 200, {"user_code": "MOCKCODE"}),
+    )
+
+    start_device_login(
+        base_url="https://example.com",
+        client_name="dvc",
+        token_name="token_name",
+        scopes=["live"]
+    )
+    assert mock_post.called
+    assert mock_post.call_args == mocker.call(
+        url='https://example.com/api/device-login',
+        json={'client_name': 'dvc', 'token_name': 'token_name', 'scopes': ['live']},
+        headers={'Content-type': 'application/json'},
+        timeout=5
+    )
+
+
+def test_check_token_authorization_expired(mocker):
+    mocker.patch("time.sleep")
+    mock_post = mocker.patch(
+        "requests.Session.post",
+        side_effect=[
+            mock_response(mocker, 400, {"detail": "authorization_pending"}),
+            mock_response(mocker, 400, {"detail": "authorization_expired"}),
+        ],
+    )
+
+    assert (
+            check_token_authorization(
+                uri="https://example.com/token_uri", device_code="random_device_code"
+            )
+            is None
+    )
+
+    assert mock_post.call_count == 2
+    assert mock_post.call_args == mocker.call(
+        "https://example.com/token_uri",
+        json={"code": "random_device_code"},
+        timeout=5,
+        allow_redirects=False,
+    )
+
+
+def test_check_token_authorization_error(mocker):
+    mocker.patch("time.sleep")
+    mock_post = mocker.patch(
+        "requests.Session.post",
+        side_effect=[
+            mock_response(mocker, 400, {"detail": "authorization_pending"}),
+            mock_response(mocker, 500, {"detail": "unexpected_error"}),
+        ],
+    )
+
+    with pytest.raises(requests.RequestException):
+        check_token_authorization(
+            uri="https://example.com/token_uri", device_code="random_device_code"
+        )
+
+    assert mock_post.call_count == 2
+    assert mock_post.call_args == mocker.call(
+        "https://example.com/token_uri",
+        json={"code": "random_device_code"},
+        timeout=5,
+        allow_redirects=False,
+    )
+
+
+def test_check_token_authorization_success(mocker):
+    mocker.patch("time.sleep")
+    mock_post = mocker.patch(
+        "requests.Session.post",
+        side_effect=[
+            mock_response(mocker, 400, {"detail": "authorization_pending"}),
+            mock_response(mocker, 400, {"detail": "authorization_pending"}),
+            mock_response(mocker, 200, {"access_token": "isat_token"}),
+        ],
+    )
+
+    assert (
+            check_token_authorization(
+                uri="https://example.com/token_uri", device_code="random_device_code"
+            )
+            == "isat_token"
+    )
+
+    assert mock_post.call_count == 3
+    assert mock_post.call_args == mocker.call(
+        "https://example.com/token_uri",
+        json={"code": "random_device_code"},
+        timeout=5,
+        allow_redirects=False,
+    )
+
+
+def mock_response(mocker, status_code, json):
+    response = Response()
+    response.status_code = status_code
+    mocker.patch.object(response, "json", side_effect=[json])
+
+    return response
